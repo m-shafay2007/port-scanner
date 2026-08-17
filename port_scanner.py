8ef92c606ba483151684b205d7002b5f12cd79fc
m-shafay2007 <shafayh04@gmail.com>
2026-08-18 01:06:29 +0500

    Update port_scanner.py
    Implemented a basic TCP port scanner using socket library. Added custom port range scanning and text file output logging for open ports.

--- port_scanner.py
+++ port_scanner.py
@@ -1,13 +1,15 @@
 import socket
-target=input("enter target ip address")
-starting_port=int(input("enter starting port number:"))
+target=input("enter target ip address: ")
+starting_port=int(input("enter starting port number: "))
 ending_port=int(input("enter ending port number: "))
-for port in range(starting_port,ending_port +1):
+print(f"\n[*] Scanning {target}...\n")
+for port in range(starting_port, ending_port  +1):
 	s=socket.socket()
-	s.settimeout(1)
+	s.settimeout(0.5)
 	result=s.connect_ex((target,port))
 	if result==0:
 		print("port",port,"--OPEN")
-	with open("result_of_port_scanner.txt","a") as f:
+			with open("result_of_port_scanner.txt","a") as f:
 		f.write("port" + str(port) + "OPEN\n")
-s.close()
+	s.close()
+print("\n[*] Scan Completed..")
